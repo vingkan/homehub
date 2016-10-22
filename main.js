789eb1b78c6ce2d150b4211e86bb76c63efe1ea7
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-10-21 23:54:25 -0500

    Add nearby services as crude get request

--- main.js
+++ main.js
@@ -20,9 +20,42 @@ function getData(query, callback, limit){
 
 var LIMIT = 200; //Only 160 data points for Homeless Services
 
+function getLocalData(){
+	var MAP_DATA = localStorage.getItem('map_data');
+	var data = JSON.parse(MAP_DATA);
+	return data;
+}
+
+function nearbyServices(){
+	getData({'$where': 'within_circle(location, ' + userLocation.latitude + ', ' + userLocation.longitude + ', ' + radius + ')'}, function(data){
+		console.log('nearby', data);
+		var nearbyDiv = document.getElementById('nearby');
+		var html = '';
+		for(var i = 0; i < data.length; i++){
+			var service = data[i];
+			html += '<p>' + service.program_model + '</p>';
+		}
+		nearbyDiv.innerHTML = html;
+	});
+}
+
 function whenOnline(){
 	getData({division: "Homeless Services"}, function(data){
-		console.log(data);
+		//Populate program models in selector
+		var optMap = {};
+		for(var d in data){
+			if(data[d]){
+				optMap[data[d].program_model] = true;
+			}
+		}
+		var selector = document.getElementById('program');
+		selector.innerHTML = '';
+		for(var i in optMap){
+			if(optMap[i]){
+				selector.innerHTML += '<option value="' + i + '">' + i + '</option>';
+			}
+		}
+		//Store data in localStorage
 		var MAP_DATA = JSON.stringify(data);
 		localStorage.setItem('map_data', MAP_DATA);
 		handleData(data);
@@ -30,23 +63,42 @@ function whenOnline(){
 }
 
 function whenOffline(){
-	var MAP_DATA = localStorage.getItem('map_data');
-	var data = JSON.parse(MAP_DATA);
+	var data = getLocalData();
 	handleData(data);
 }
 
-function handleData(data){
+function handleData(data, filterFn){
+	var filterPoint = filterFn || function(input){
+		return true;
+	}
 	var markerList = data.map(function(item){
-		var point = {
-			name: item.agency,
-			getLat: function(){
-				return parseFloat(item.latitude, 10);
-			},
-			getLon: function(){
-				return parseFloat(item.longitude, 10);
+		if(filterPoint(item)){
+			var point = {
+				name: item.program_model + ': ' + item.agency,
+				getLat: function(){
+					return parseFloat(item.latitude, 10);
+				},
+				getLon: function(){
+					return parseFloat(item.longitude, 10);
+				}
 			}
+			return point;
 		}
-		return point;
+		else{
+			return false;
+		}
+	});
+	var markerList = markerList.filter(function(item){
+		return item;
 	});
 	initGoogleMap(markerList);
+}
+
+function filterProgram(){
+	var selector = document.getElementById('program');
+	console.log(selector.value);
+	var data = getLocalData();
+	handleData(data, function(item){
+		return item.program_model === selector.value;
+	});
 }
--- map.js
+++ map.js
@@ -11,36 +11,42 @@ function updateCoords(position){
 navigator.geolocation.getCurrentPosition(updateCoords);
 
 function initGoogleMap(markerArray){
-	var centerPoint = markerArray[0];
-	var mapProperties = {
-		//center: new google.maps.LatLng(userLocation.latitude, userLocation.longitude),
-		center: new google.maps.LatLng(centerPoint.getLat(), centerPoint.getLon()),
-		zoom: 14,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var mapDiv = document.getElementById('googleMap');
-	var googleMap = new google.maps.Map(mapDiv, mapProperties);
-	var oms = new OverlappingMarkerSpiderfier(googleMap);
-	var infoWindow = new google.maps.InfoWindow();
-	oms.addListener('click', function(marker, event){
-		infoWindow.setContent(marker.desc);
-		infoWindow.open(map, marker);
-	});
-	oms.addListener('spiderify', function(markers){
-		infoWindow.close();
-	});
-
-	for(var m = 0; m < markerArray.length; m++){
-		var current = markerArray[m];
-		var marker = new google.maps.Marker({
-			position: {
-				lat: current.getLat(),
-				lng: current.getLon()
-			},
-			map: googleMap,
-			title: current.name
+	if(markerArray.length > 0){
+		var centerPoint = markerArray[0];
+		var mapProperties = {
+			//center: new google.maps.LatLng(userLocation.latitude, userLocation.longitude),
+			center: new google.maps.LatLng(centerPoint.getLat(), centerPoint.getLon()),
+			zoom: 14,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var mapDiv = document.getElementById('googleMap');
+		var googleMap = new google.maps.Map(mapDiv, mapProperties);
+		var oms = new OverlappingMarkerSpiderfier(googleMap);
+		var infoWindow = new google.maps.InfoWindow();
+		oms.addListener('click', function(marker, event){
+			infoWindow.setContent(marker.desc);
+			infoWindow.open(map, marker);
 		});
-		oms.addMarker(marker);
+		oms.addListener('spiderify', function(markers){
+			infoWindow.close();
+		});
+
+		for(var m = 0; m < markerArray.length; m++){
+			var current = markerArray[m];
+			if(current){
+				var marker = new google.maps.Marker({
+					position: {
+						lat: current.getLat(),
+						lng: current.getLon()
+					},
+					map: googleMap,
+					title: current.name
+				});
+				oms.addMarker(marker);
+			}
+		}
 	}
-	console.log('done');
+	else{
+		console.warn("Empty markerArray list.");
+	}
 }
